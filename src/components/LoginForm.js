71318d89cdced2ee369520222e61908f9cef5870
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2017-09-27 14:00:23 +0900

    Login form and email input field included

---
+++ src/components/LoginForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Button, Card, CardSection, Input } from "./common";
+
+class LoginForm extends Component {
+
+    state = { email: "" };
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
+                        label="Email"
+                        placeholder="user@gmail.com"
+                    />
+                </CardSection>
+
+                <CardSection />
+
+                <CardSection>
+                    <Button>
+                        Login
+                    </Button>
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+export default LoginForm;
