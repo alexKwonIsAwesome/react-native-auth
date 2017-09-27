24834e70d299f0ad10e2f76a269d89e61355195b
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2017-09-27 11:18:48 +0900

    added firebase and set up

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import firebase from "firebase";
 import { Header } from "./components/common";
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyCYJ97CVIrZv6mgGlb1_PwW0Kg7IhkX5Dg",
+            authDomain: "authentication-e04e4.firebaseapp.com",
+            databaseURL: "https://authentication-e04e4.firebaseio.com",
+            projectId: "authentication-e04e4",
+            storageBucket: "authentication-e04e4.appspot.com",
+            messagingSenderId: "91235170341"
+        });
+    }
+
     render() {
         return (
             <View>
